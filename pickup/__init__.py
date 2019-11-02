1a83e9636682fc1de15238d6b68d7a30ecdb951a
Aaron <aaronpham0103@gmail.com>
2019-11-02 15:28:25 -0400

    updates backend connection to file

--- pickup/__init__.py
+++ pickup/__init__.py
@@ -1,30 +1,14 @@
 import os
-import json
-from flask import Flask, render_template
-from pymongo import MongoClient
+from flask import Flask, send_from_directory
 
-def load_mongo(db:str()):
-    password = input('Password: ')
-    client = MongoClient("mongodb+srv://aar0npham:{}@food-izclc.mongodb.net/test?retryWrites=true&w=majority".format(password))
-    db = client[db]
-    return db
-
-def write_db(db, col, file):
-    dump = load_mongo(db)
-    cur = dump[col].find({})
-    count = dump[col].count_document({})
-    with open(file, 'w') as f:
-        f.write('[')
-        for i, doc in enumerate(cur,1):
-            f.write(json.dump(doc))
-            if i != count:
-                f.write(',')
-        f.write(']')
-    f.close()
-
-def create_config(test_config=None):
+def create_app(test_config=None):
+    # create and configure the app
+    # Database saved mongo to json
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(SECRET_KEY='dev',DATABASE=os.path.join(app.instance_path,'pickup.json'))
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'pickup.json'),
+    )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -39,8 +23,8 @@ def create_config(test_config=None):
     except OSError:
         pass
 
-    @app.route('/', methods=['GET'])
-    def index():
-        return render_template('index.html')
+    @app.route('/')
+    def hello():
+        return send_from_directory('templates','index.html')
 
     return app
